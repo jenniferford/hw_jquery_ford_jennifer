d9cfaeefd51b96f6742df3c824ba098567f9b67e
[Jennifer Ford] <[jennifer.ford.design@gmail.com]>
2015-11-20 21:44:49 -0500

    assigned variable to p tags, changed styles of p variables

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	var paragraphs = $( 'p' );
+	paragraphs.css({
+        "color": "DarkBlue",
+        "background-color": "DarkGray",
+        "font-family": "Arial Black"
+    });
+});
